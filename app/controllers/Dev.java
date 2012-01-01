fcd197f1a0446c7c59c6cc0c88c9797db13c2f6d
Amy Boyd <amy@amyboyd.co.uk>
2012-01-01 14:35:59 +0000

    deps.js

---
+++ app/controllers/Dev.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.*;
+
+/**
+ * Development-only actions.
+ */
+public class Dev extends Controller {
+    public static void closureDepsJS() {
+        ok();
+    }
+}
